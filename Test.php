875e2e88cf32b8d2917a2e2628f87b54c3a3c43f
iChen <st.chenlei@gmail.com>
2015-09-08 13:18:52 +0800

    run forever with output

--- Test.php
+++ Test.php
@@ -3,8 +3,11 @@
 class Test extends PHPUnit_Framework_TestCase
 {
   public function testOnePlusOne() {
+    $a = 1;
     while(true) {
       $this->assertEquals(1+1,2);
+      print "this is successful! {$a}";
+      $a = $a + 1;
     }
   	}
 }
